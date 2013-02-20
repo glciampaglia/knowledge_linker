d4828b61faec68586c0c450bb9d99eab3328914d
Karissa McKelvey <krmckelv@gmail.com>
2013-02-20 17:09:52 -0500

    Update neo4j/test.py
    Former-commit-id: d032fd9811c6b83d13c668332ce51ff41083c9dc

--- neo4j/test.py
+++ neo4j/test.py
@@ -1,11 +1,22 @@
 from py2neo import neo4j, gremlin
 
-g = neo4j.GraphDatabaseService("http://carl.cs.indiana.edu:7474/db/data/")
+def truthiness(entity, predicate, object):
+    """Takes an rdf triple and performs inference or traversal and continuous value between 0 and 1
+    """
 
-script = """
-v = g.idx(T.v)[[value:'U http://dbpedia.org/resource/Barack_Obama']]
-v
-"""
+    g = neo4j.GraphDatabaseService("http://carl.cs.indiana.edu:7474/db/data/")
+    
+    script = """
+    v = g.idx(T.v)[[value:'U http://dbpedia.org/resource/Barack_Obama']]
+    v
+    """
+    
+    result = gremlin.execute(script, g)
+    print result
 
-result = gremlin.execute(script, g)
-print result
+if __name__ == "__main__":
+    entity = "http://dbpedia.org/resource/Barack_Obama"
+    pred = "rdf:type"
+    object =  "http://dbpedia.org/resource/President_of_the_United_States"
+    result = truthiness(entity, pred, object)
+    print result
