glciampaglia/knowledge_linker

commit ee313a6a2b7b6f89ded62a2f271efa3d0354dc92

now producing only paths of given length at each iteration + added shell script for collecting stats
Former-commit-id: ad842594578767e17ec6ae70603855825508ecb2

neo4j/paths.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import datetime
 from argparse import ArgumentParser, FileType
 from py2neo import neo4j, gremlin
@@ -14,7 +15,7 @@ def paths(source, target, predicate=None, maxlen=1):
     script = """
     v = g.idx('vertices')[[value:'U %s']]
     y = []
-    v.as('x').outE.inV.loop('x'){it.loops < %d}.filter{it.value == 'U %s'}.paths{it.value}{it.p}
+    v.as('x').outE.inV.loop('x'){it.loops == %d}.filter{it.value == 'U %s'}.paths{it.value}{it.p}
     """
 
     result = gremlin.execute(script % (source, maxlen, target), g)
@@ -32,8 +33,6 @@ def iteratepathlen(ns):
         # paths from source to target following out-edges
         start_time = datetime.now()
         result = paths(ns.source, ns.target, maxlen=l)
-        if ns.debug:
-            pprint(result)
         stop_time = datetime.now()
         print >> ns.output, stop_time - start_time,
 
@@ -41,8 +40,6 @@ def iteratepathlen(ns):
         # -> target following in-edges)
         start_time = datetime.now()
         result2 = paths(ns.target, ns.source, maxlen=l)
-        if ns.debug:
-            pprint(result2)
         stop_time = datetime.now()
         print >> ns.output, stop_time - start_time,
         
@@ -54,6 +51,11 @@ def iteratepathlen(ns):
         print >> ns.output, len(result2),
         print >> ns.output, "NA"
 
+        if ns.debug:
+            print >> sys.stderr, "# len = %d" % l
+            pprint(result, stream=sys.stderr)
+            pprint(result2, stream=sys.stderr)
+            print >> sys.stderr, ""
 
 if __name__ == "__main__":
     def_source = "http://dbpedia.org/resource/Barack_Obama"

neo4j/pathsstats.sh
@@ -1,0 +1,19 @@
+#!/bin/bash
+
+STATEMENTS=statements.txt
+SCRIPT=paths.py
+MAXLEN=20
+PREF=http://dbpedia.org/resource/
+
+exec 3<$STATEMENTS
+
+while `true` ; do
+    read -u 3 source target
+    if [[ $? != 0 ]] ; then
+        break
+    fi
+    echo "# ---------------------------------------------------" | tee /dev/stderr
+    echo "# ${source##$PREF} -> ${target##$PREF}" | tee /dev/stderr
+    echo "# ---------------------------------------------------" | tee /dev/stderr
+    python $SCRIPT -l $MAXLEN --debug -s $source -t $target 
+done

commit 4167576ceab5e9559d8175e25b9b7c0eb9e2b5a8

fixed Gremlin loop condition bug introduced in ee313a6a2b7b6f89ded62a2f271efa3d0354dc92 [formerly ad842594578767e17ec6ae70603855825508ecb2]
Former-commit-id: 62fb04d4d77f75cf0a530b240cc2229e250a0a43

neo4j/paths.py
@@ -15,7 +15,7 @@ def paths(source, target, predicate=None, maxlen=1):
     script = """
     v = g.idx('vertices')[[value:'U %s']]
     y = []
-    v.as('x').outE.inV.loop('x'){it.loops == %d}.filter{it.value == 'U %s'}.paths{it.value}{it.p}
+    v.as('x').outE.inV.loop('x'){it.loops <= %d}.filter{it.value == 'U %s'}.paths{it.value}{it.p}
     """
 
     result = gremlin.execute(script % (source, maxlen, target), g)