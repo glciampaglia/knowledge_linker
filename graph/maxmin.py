de20bd8ec50ee2edc7febe72770988c04651dc1b
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2013-05-23 13:16:28 -0400

    Sparse algorithm (CSR matrix) for maxmin product implemented in pure Python
    Former-commit-id: 581f0d0a43dba7c92b7ccfc3a3b761c1dca67345

--- graph/maxmin.py
+++ graph/maxmin.py
@@ -36,53 +36,51 @@ def maxmin_sparse(A):
         A = sp.csr_matrix(A)
     N, M = A.shape
     AP = sp.dok_matrix(A.shape, A.dtype)
-    At = A.transpose().tocsr() # transpose 
+    At = A.transpose().tocsr() # transpose of A in CSR format
 
     for i in xrange(N):
-        # ii is the starting index in A.data/A.indices for the non-zero elements
-        # in the i-th row
-        ii = A.indptr[i] 
-        iimax = A.indptr[i + 1]
     
         for j in xrange(M):
             
-            # jj is the starting index in At.data/At.indices for the non-zero
-            # elements in the j-th row
+            # ii is the index of the first non-zero element value (in A.data)
+            # and column index (in A.indices) of the the i-th row
+            ii = A.indptr[i] 
+            iimax = A.indptr[i + 1]
+
+            # jj is the index of the first non-zero element value (in At.data)
+            # and column (that is, row) index (in A.indices) of the the j-th row
+            # (that is, column).
             jj = At.indptr[j]
             jjmax = At.indptr[j + 1]
 
-            if (ii == iimax) or (jj == jjmax):
-                # all elements in either the row or the column (or both) are
-                # zero, so we can skip the pairwise mins
-                max_ij = 0.
+            max_ij = 0.
+            
+            while (ii < iimax) and (jj < jjmax):
+                
+                ik = A.indices[ii]
+                kj = At.indices[jj]
 
-            else:
-                max_ij = 0
+                if ik == kj: 
+                    # same element, apply min
+                    min_k = min(A.data[ii], At.data[jj])
+                    # update the maximum so far
+                    if min_k > max_ij:
+                        max_ij = min_k
+                    ii += 1
+                    jj += 1
 
-                while (ii < iimax) or (jj < jjmax):
-                    ik = A.indices[ii]
-                    kj = At.indices[jj]
+                elif ik > kj: 
+                    # the row element (in A) corresponding to kj is zero,
+                    # hence min_k is zero. Advance only jj.
+                    jj += 1
 
-                    if ik == kj: # same element, apply min
-                        # get the minimum
-                        min_k = min(A.data[ii], At.data[jj])
-                        # update the maximum so far
-                        if min_k > max_ij:
-                            max_ij = min_k
-                        ii += 1
-                        jj += 1
+                else: # ik < kj
+                    # the column elment (in At) corresponding to ik is zero,
+                    # hence min_k is zero. Advance only ii.
+                    ii += 1
 
-                    elif ik > kj: 
-                        # the row element (in A) corresponding to kj is zero,
-                        # hence min_k is zero. Advance only jj.
-                        jj += 1
-
-                    else: # ik < kj
-                        # the column elment (in At) corresponding to ik is zero,
-                        # hence min_k is zero. Advance only ii.
-                        ii += 1
-
-            AP[i,j] = max_ij
+            if max_ij:
+                AP[i,j] = max_ij
 
     # return in CSR format
     return AP.tocsr()
--- graph/test_maxmin.py
+++ graph/test_maxmin.py
@@ -4,17 +4,24 @@ from numpy.random import seed
 from nose.tools import raises
 
 # local imports
-from maxmin import maxmin_naive, _maxmin_naive
+from maxmin import maxmin_naive, _maxmin_naive, maxmin_sparse
 
 def test_naive():
-    A = np.random.rand(5,5)
+    A = np.random.rand(5, 5)
     AP = maxmin_naive(A)
     AP2 = _maxmin_naive(A) 
     assert np.array_equal(AP, AP2)
 
 @raises(ValueError)
 def test_naive_sparse():
-    A = sp.rand(5,5,.2, 'csr')
+    A = sp.rand(5, 5, .2, 'csr')
     AP = maxmin_naive(A)
     AP2 = _maxmin_naive(A) # expects ndarray type
     assert np.array_equal(AP, AP2)
+
+def test_sparse_slow():
+    A = sp.rand(5, 5, .2, 'csr')
+    AP = maxmin_naive(A)
+    AP2 = maxmin_sparse(A)
+    assert np.array_equal(AP, AP2.todense())
+
