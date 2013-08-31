80abcbfeb50b6826af610211bf25b62511508060
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2013-08-30 22:57:07 -0400

    Added -Wall and -O compile flags to debugging distutils script

--- setup_gdb.py
+++ setup_gdb.py
@@ -3,6 +3,7 @@ from Cython.Distutils import build_ext, Extension
 from numpy import get_include
 
 _incl = [ get_include() ]
+_args = ['-Wall', '-O']
 
 setup(
         name="truthy_measure",
@@ -14,9 +15,11 @@ setup(
         cmdclass={'build_ext' : build_ext},
         ext_modules=[
             Extension("truthy_measure.heap", ["truthy_measure/heap.pyx",],
+                extra_compile_args=_args,
                 include_dirs=_incl,
                 pyrex_gdb=True),
             Extension("truthy_measure.cmaxmin", [ "truthy_measure/cmaxmin.pyx", ],
+                extra_compile_args=_args,
                 include_dirs=_incl, 
                 pyrex_gdb=True) 
             ],
