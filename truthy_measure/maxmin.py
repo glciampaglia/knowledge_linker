4bd6daf4091b948b4a323c8bceec774d3f32419f
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2013-09-04 18:38:57 -0400

    Now immediately adding data files to uncompressed TAR archive

--- truthy_measure/maxmin.py
+++ truthy_measure/maxmin.py
@@ -59,7 +59,6 @@ traversal algorithms.
 from __future__ import division
 import os
 import sys
-import atexit
 import numpy as np
 import scipy.sparse as sp
 from multiprocessing import Pool, Array, cpu_count, current_process
@@ -70,6 +69,7 @@ from itertools import izip
 from operator import itemgetter
 from heapq import heappush, heappop, heapify
 from subprocess import call
+from tarfile import open as opentar
 
 now = datetime.now
 
@@ -140,36 +140,38 @@ max_tasks_per_worker = 500
 log_out = 'bottleneck_dists-{proc:0{width}d}.log'
 log_out_start = 'bottleneck_dists_{start:0{width1}d}-{{proc:0{{width}}d}}.log'
 logline = "{now}: worker-{proc:0{width}d}: source {source} completed."
+tar_out = 'bp_{proc:0{{width}}d}.tar'
+tar_out_start = 'bp_{start:0{width1}d}-{{proc:0{{width}}d}}.tar'
 
 _nprocs = None
-_logf = None
 _logpath = None
+_tarpath = None
 _dirtree = None
-
-@atexit.register
-def _atexit_worker():
-    if _logf is not None:
-        _logf.flush()
-        _logf.close()
 
 def _bottleneck_worker(n):
     global _A, _dirtree, _logpath, _logf, _nprocs, digits_procs
     N = _A.shape[0]
     worker_id, = current_process()._identity
-    if _logf is None:
-        path = _logpath.format(proc=worker_id, width=digits_procs)
-        _logf = open(path, 'a', 1) # line buffered
+    logpath = _logpath.format(proc=worker_id, width=digits_procs)
+    tarpath = _tarpath.format(proc=worker_id, width=digits_procs)
     outpath = _dirtree.getleaf(n)
-    with closing(open(outpath, 'w')) as outf:
+    with \
+            closing(open(outpath, 'w')) as outf,\
+            closing(opentar(tarpath, 'a')) as tarf,\
+            closing(open(logpath, 'a', 1)) as logf:
         dists, paths = cbottleneckpaths(_A, n, outf)
-    print >> _logf, logline.format(now=now(), source=n, proc=worker_id,
-            width=digits_procs)
+        tarf.add(outpath)
+        os.remove(outpath)
+        logf.write(logline.format(now=now(), source=n, proc=worker_id,
+                width=digits_procs) + '\n')
 
-def _init_worker_dirtree(nprocs, logpath, dirtree, indptr, indices, data, shape):
-    global _dirtree, _logpath, _nprocs, digits_procs, digits_rows
+def _init_worker_dirtree(nprocs, tarpath, logpath, dirtree, indptr, indices, data,
+        shape):
+    global _dirtree, _logpath, _nprocs, digits_procs, digits_rows, _tarpath
     _nprocs = nprocs
     digits_procs = int(np.ceil(np.log10(_nprocs)))
     _logpath = logpath
+    _tarpath = tarpath
     _dirtree = dirtree
     _init_worker(indptr, indices, data, shape)
 
@@ -230,9 +232,11 @@ def parallel_bottleneckpaths(A, dirtree, start=None, offset=None, nprocs=None):
     data = Array(c_double, A.data)
     if start is None:
         logpath = log_out
+        tarpath = tar_out
     else:
         logpath = log_out_start.format(start=start, width1=digits)
-    initargs = (nprocs, logpath, dirtree, indptr, indices, data, A.shape)
+        tarpath = tar_out_start.format(start=start, width1=digits)
+    initargs = (nprocs, tarpath, logpath, dirtree, indptr, indices, data, A.shape)
     print '{}: launching pool of {} workers.'.format(now(), nprocs)
     pool = Pool(processes=nprocs, initializer=_init_worker_dirtree,
             initargs=initargs, maxtasksperchild=max_tasks_per_worker)
