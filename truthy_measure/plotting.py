048f61a0e34bd8689a4cc97b9a863b55423bbc5d
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2015-05-09 13:59:21 -0400

    By default copy data array in plot_cdf, choose between fractional ranking and normal ranking (produces larger plots), and added example plot in __main__ block; small PEP8 fixes

--- truthy_measure/plotting.py
+++ truthy_measure/plotting.py
@@ -6,35 +6,49 @@ from itertools import groupby
 from operator import itemgetter
 
 
-def plot_cdf(x, **kwargs):
+def plot_cdf(x, copy=True, fractional=True, **kwargs):
     """
-    Add a log-log CDF plot to the current axes.
+    Add a log-log CCDF plot to the current axes.
 
     Arguments
     ---------
     x : array_like
         The data to plot
 
+    copy : boolean
+        copy input array in a new object before sorting it. If data is a *very*
+        large, the copy can avoided by passing False to this parameter.
+
+    fractional : boolean
+        compress the data by means of fractional ranking. This collapses the
+        ranks from multiple, identical observations into their midpoint, thus
+        producing smaller figures. Note that the resulting plot will NOT be the
+        exact CCDF function, but an approximation.
+
     Additional keyword arguments are passed to `matplotlib.pyplot.loglog`.
     Returns a matplotlib axes object.
 
-    This version tries collapse the ranks from multiple, identical observations
-    into their midpoint. This produces smaller figures.
-
     """
     N = float(len(x))
+    if copy:
+        x = x.copy()
     x.sort()
-    t = []
-    for x, chunk in groupby(enumerate(x, 1), itemgetter(1)):
-        xranks, _ = zip(*list(chunk))
-        t.append((float(x), xranks[0] + len(xranks) / 2.))
-    t = np.asarray(t)
+    if fractional:
+        t = []
+        for x, chunk in groupby(enumerate(x, 1), itemgetter(1)):
+            xranks, _ = zip(*list(chunk))
+            t.append((float(x), xranks[0] + np.ptp(xranks) / 2.0))
+        t = np.asarray(t)
+    else:
+        t = np.c_[np.asfarray(x), np.arange(N) + 1]
+    print t[-2:]
     if 'ax' not in kwargs:
         ax = plt.gca()
     else:
         ax = kwargs.pop('ax')
     ax.loglog(t[:, 0], (N - t[:, 1]) / N, 'ow', **kwargs)
     return ax
+
 
 def plot_pdf_log2(x, nbins=10, **kwargs):
     '''
@@ -53,7 +67,6 @@ def plot_pdf_log2(x, nbins=10, **kwargs):
     x = np.asarray(x)
     exp_max = np.ceil(np.log2(x.max()))
     bins = np.logspace(0, exp_max, exp_max + 1, base=2)
-    fig = plt.gcf()
     ax = plt.gca()
     hist, _ = np.histogram(x, bins=bins)
     binsize = np.diff(np.asfarray(bins))
@@ -61,3 +74,23 @@ def plot_pdf_log2(x, nbins=10, **kwargs):
     ax.loglog(bins[1:], hist, 'ow', **kwargs)
     return ax
 
+if __name__ == '__main__':
+    from scipy.stats import zipf
+    x = zipf.rvs(2, size=10000)
+    plt.figure(figsize=(9, 3))
+    plt.subplot(131)
+    plot_cdf(x, fractional=False)
+    plt.title("CCDF")
+    ylim = plt.ylim()
+
+    plt.subplot(132)
+    plot_cdf(x)
+    plt.title("CCDF with fractional ranks")
+    plt.ylim(*ylim)
+
+    plt.subplot(133)
+    plot_pdf_log2(x)
+    plt.title("PDF")
+
+    plt.tight_layout()
+    plt.show()
