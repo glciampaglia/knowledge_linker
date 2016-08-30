a4037bcd1bd89a60abae570742f484dacfc4419e
Giovanni Luca Ciampaglia <gciampag@indiana.edu>
2016-08-30 15:30:06 -0400

    PEP8 fixes

--- knowledge_linker/__init__.py
+++ knowledge_linker/__init__.py
@@ -1,22 +1,18 @@
-import os
 import sys
 import warnings
 
-# filter out some harmless warnings
-
+# filter out some harmless warnings about SciPy's CSR matrix format.
 warnings.filterwarnings('ignore',
         message='.*',
         module='scipy\.sparse\.compressed.*',
         lineno=122)
 
-# Format warnings nicely
 
+# Format warnings nicely
 def _showwarning(message, category, filename, lineno, line=None):
-    filename = os.path.basename(__file__)
     warning = category.__name__
     print >> sys.stderr
     print >> sys.stderr, '>> {}: {}'.format(warning, message)
     print >> sys.stderr
 
 warnings.showwarning = _showwarning
-
